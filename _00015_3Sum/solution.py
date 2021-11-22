b4c09ad1a1b32fbf4e7765b4d2ab9df7d18ff0e2
uuboyscy <u.shi@lndata.com>
2021-11-22 22:56:59 +0800

    LeetCode 15: Three Sum

--- _00015_3Sum/solution.py
+++ _00015_3Sum/solution.py
@@ -37,19 +37,20 @@ class Solution:
     #
     #     return outputList
     def threeSum(self, nums):
-        outputList = []
         if nums.__len__() < 3:
-            return outputList
+            return []
 
+        outputList = []
         nums.sort()
         numsSet = set(nums)
 
         for fixedNumber in numsSet:
-            idxLeftNum = nums.index(fixedNumber)
+            idxLeftNum = nums.index(fixedNumber) + 1
             idxRightNum = nums.__len__() - 1
+            if idxLeftNum > nums.__len__() - 2: continue
             leftNum = nums[idxLeftNum]
             rightNum = nums[idxRightNum]
-            while idxLeftNum != idxRightNum:
+            while idxLeftNum < idxRightNum:
                 threeNumsSum = fixedNumber + leftNum + rightNum
                 if threeNumsSum < 0:
                     idxLeftNum += 1
@@ -58,11 +59,11 @@ class Solution:
                 elif threeNumsSum == 0:
                     idxLeftNum += 1
                     idxRightNum -= 1
-                    outputList.append([fixedNumber, leftNum, rightNum])
-                while nums[idxLeftNum] == nums[idxLeftNum - 1]:
-                    idxLeftNum += 1
-                while nums[idxRightNum] == nums[idxRightNum - 1]:
-                    idxRightNum -= 1
+                    if not (outputList.__len__() > 0 and [fixedNumber, leftNum, rightNum] == outputList[-1]):
+                        outputList.append([fixedNumber, leftNum, rightNum])
+
+                leftNum = nums[idxLeftNum]
+                rightNum = nums[idxRightNum]
 
         return outputList
 
@@ -73,4 +74,5 @@ if __name__ == '__main__':
     print(s.threeSum([-1,0,1,0])) # [[-1, 0, 1]]
     print(s.threeSum([3,0,-2,-1,1,2])) # [[-2,-1,3],[-2,0,2],[-1,0,1]]
     print(s.threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4])) # [[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
-
+    print(s.threeSum([0,0,0])) # [[0,0,0]]
+    print(s.threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6])) # [[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
