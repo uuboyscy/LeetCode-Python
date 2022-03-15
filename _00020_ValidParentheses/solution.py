e671453c3f658cd6567a6bcef307031fd5b66abb
allenshi <allenshi@gamania.com>
2022-03-15 14:09:16 +0800

    20 2 ways solved

--- _00020_ValidParentheses/solution.py
+++ _00020_ValidParentheses/solution.py
@@ -9,12 +9,20 @@ Open brackets must be closed in the correct order.
 
 
 class Solution:
+    """
+    Runtime: 72 ms, faster than 5.55% of Python3 online submissions for Valid Parentheses.
+    Memory Usage: 13.7 MB, less than 98.66% of Python3 online submissions for Valid Parentheses.
+    """
     def isValid(self, s: str) -> bool:
         while "()" in s or "[]" in s or "{}" in s:
             for p in ("()", "[]", "{}"):
                 s = s.replace(p, "")
         return s.__len__() == 0
 
+    """
+    Runtime: 24 ms, faster than 98.89% of Python3 online submissions for Valid Parentheses.
+    Memory Usage: 14 MB, less than 44.15% of Python3 online submissions for Valid Parentheses.
+    """
     def isValid(self, s: str) -> bool:
         symbolMap = {
             "(": 1,
@@ -24,17 +32,19 @@ class Solution:
             "]": -2,
             "}": -3
         }
-        sList = list(s)
-        stackList = [symbolMap[sList[0]]]
-        sSum = 0
+        sList = list(s)  # List<String>
+        stackList = [symbolMap[sList[0]]]  # List<Integer>
 
         for symbol in sList[1:]:
-            if symbolMap[symbol] < 0 and symbolMap[symbol] + stackList[-1] == 0:
+            isNewSymbolLtZero = symbolMap[symbol] < 0
+            if isNewSymbolLtZero and stackList.__len__() == 0:
+                stackList.append(symbolMap[symbol])
+            elif isNewSymbolLtZero and symbolMap[symbol] + stackList[-1] == 0:
                 del stackList[-1]
-            elif symbolMap[symbol] < 0:
+            elif isNewSymbolLtZero:
                 return False
             else:
-                stackList.append(symbol)
+                stackList.append(symbolMap[symbol])
 
         return stackList == []
 
@@ -43,8 +53,8 @@ class Solution:
 if __name__ == '__main__':
     s = Solution()
 
-    print(s.isValid("()")) ## True
-    print(s.isValid("()[]{}")) ## True
-    print(s.isValid("(]")) ## False
-    print(s.isValid("()[{]}"))
+    print("()", s.isValid("()")) ## True
+    print("()[]{}", s.isValid("()[]{}")) ## True
+    print("(]", s.isValid("(]")) ## False
+    print("()[{]}", s.isValid("()[{]}"))
 
