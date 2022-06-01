3ab9dfcf7f54fd138ad0392e19af71d4f44ac529
uuboyscy <aegis12321@gmail.com>
2022-06-01 17:07:58 +0800

    25 solved

--- _00025_ReverseNodesInKGroup/solution.py
+++ _00025_ReverseNodesInKGroup/solution.py
@@ -6,50 +6,48 @@ k is a positive integer and is less than or equal to the length of the linked li
 You may not alter the values in the list's nodes, only nodes themselves may be changed.
 """
 
+"""
+Runtime: 66 ms, faster than 51.87% of Python3 online submissions for Reverse Nodes in k-Group.
+Memory Usage: 15.8 MB, less than 7.43% of Python3 online submissions for Reverse Nodes in k-Group.
+"""
+
 from typing import Optional
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        def list2LinkedList(inputList: list) -> ListNode:
-            outputLinkedList = None
-            tmpLinkedList = None
-            if inputList == None or inputList.__len__() == 0:
-                return None
-            elif inputList.__len__() == 1:
-                return ListNode(val=inputList[0])
-            for n, i in enumerate(inputList[::-1]):
-                if n == 0:
-                    tmpLinkedList = ListNode(i)
-                else:
-                    outputLinkedList = ListNode(i)
-                    outputLinkedList.next = tmpLinkedList
-                    tmpLinkedList = outputLinkedList
+        if head == None or k <= 1:
+            return head
 
-            return outputLinkedList
+        # Original Node
+        backupHead = head
 
+        # Create last node in this partial reversed node
+        lastNode = ListNode(val=head.val)
+
+        # Create first reversed node
+        head = head.next
         if head == None:
-            return None
-        outputListNode = None
-        outputList = list()
+            return backupHead
+        outputListNode = ListNode(val=head.val, next=lastNode)
 
-        i = 0
+        i = 2
         while i < k:
-            if head == None:
-                break
-            outputList.append(head.val)
             head = head.next
+            if head == None:
+                return backupHead
+            outputListNode = ListNode(val=head.val, next=outputListNode)
             i += 1
 
-        if i == k - 1:
-            outputListNode = list2LinkedList(outputList.reverse())
-            outputListNode.next = self.reverseKGroup(head, k)
-            return outputListNode
-        else:
-            return list2LinkedList(outputList)
+        lastNode.next = self.reverseKGroup(head=head.next, k=k)
+
+        return outputListNode
 
 
 if __name__ == '__main__':
