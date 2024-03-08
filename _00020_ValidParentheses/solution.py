97f7cc1d42cd25502c3e3e4f033e277cd2ceb243
uuboyscy <aegis12321@gmail.com>
2024-03-08 23:07:24 +0800

    20

--- _00020_ValidParentheses/solution.py
+++ _00020_ValidParentheses/solution.py
@@ -48,6 +48,34 @@ class Solution:
 
         return stackList == []
 
+    def isValid(self, s: str) -> bool:
+        symbol_map = {
+            "(": 1,
+            "[": 2,
+            "{": 3,
+            ")": -1,
+            "]": -2,
+            "}": -3
+        }
+        if s and (first_symbol := symbol_map[s[0]]) > 0:
+            stack = [first_symbol]
+        else:
+            return False
+
+        for symbol in s[1:]:
+            if stack and symbol_map[symbol] + stack[-1] < 0:
+                return False
+            if stack and symbol_map[symbol] + stack[-1] > 0 and symbol_map[symbol] < 0:
+                return False
+            if stack and symbol_map[symbol] + stack[-1] == 0 and symbol_map[symbol] > 0:
+                return False
+            if stack and symbol_map[symbol] + stack[-1] == 0 and symbol_map[symbol] < 0 :
+                stack.pop()
+                continue
+
+            stack.append(symbol_map[symbol])
+
+        return stack == []
 
 
 if __name__ == '__main__':
