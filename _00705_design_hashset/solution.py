06e16cf3d163d7582730e8ef1d17fc3ef721f600
uuboyscy <aegis12321@gmail.com>
2024-03-08 23:27:01 +0800

    705

---
+++ _00705_design_hashset/solution.py
@@ -0,0 +1,31 @@
+"""
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+"""
+
+class MyHashSet:
+
+    def __init__(self):
+        self.hash_set = set()
+
+    def add(self, key: int) -> None:
+        self.hash_set.add(key)
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.hash_set.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.hash_set
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
